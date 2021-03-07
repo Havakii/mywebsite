92fa3d5d83939e15b1737e926032e6120418261d
Havakii <80125324+Havakii@users.noreply.github.com>
2021-03-07 16:21:36 +0100

    Website_modifications

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-alert('Bienvenue sur le site');
-window.onscroll = function (){
-	//ici les instruction 
+window.onscroll = function (){modalHomePage()};
+document.getElementById("leadModal");
+var modal = document.getElementById("leadModal");
+var btn = document.getElementById("btnNo");
+console.log(modal, 'console log de modal');
+function modalHomePage(){
+	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350){
+	modal.style.display = "block";
+	window.onscroll = null;
+	}
+}
+
+btn.onclick= function(){
+	modal.style.display = "none";
 }
